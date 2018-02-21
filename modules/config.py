52e0a9e7fdd7061791343c6c1395ae05554a4777
Marc Hanheide <marc@hanheide.net>
2018-02-21 11:17:25 +0000

    config based on env

--- modules/config.py
+++ modules/config.py
@@ -1,12 +1,13 @@
 import os
 
-listen_port = 5000
-
 if 'DB_PORT_27017_TCP_ADDR' in os.environ:
     mongo_host = os.environ['DB_PORT_27017_TCP_ADDR']
-    mongo_port = 27017
 else:
     mongo_host = 'localhost'
+
+if 'DB_PORT_27017_TCP_PORT' in os.environ:
+    mongo_port = int(os.environ['DB_PORT_27017_TCP_PORT'])
+else:
     mongo_port = 27017
 
 if 'QV_BASE_URL' in os.environ:
@@ -14,5 +15,10 @@ if 'QV_BASE_URL' in os.environ:
 else:
     base_url = "http://localhost:5000/"
 
- # dummy data to stop proxy buffering
+if 'QV_PORT' in os.environ:
+    listen_port = int(os.environ['QV_PORT'])
+else:
+    listen_port = 5000
+
+# dummy data to stop proxy buffering
 dummy_data = range(0, 20480)
