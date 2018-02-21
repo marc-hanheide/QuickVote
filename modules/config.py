3bf704a3a4bb1afd74a0a7ba4172656a8416dbc5
Marc Hanheide <marc@hanheide.net>
2018-02-21 11:21:35 +0000

    auto base_url takes listen port into account

--- modules/config.py
+++ modules/config.py
@@ -10,15 +10,15 @@ if 'DB_PORT_27017_TCP_PORT' in os.environ:
 else:
     mongo_port = 27017
 
-if 'QV_BASE_URL' in os.environ:
-    base_url = os.environ['QV_BASE_URL']
-else:
-    base_url = "http://localhost:5000/"
-
 if 'QV_PORT' in os.environ:
     listen_port = int(os.environ['QV_PORT'])
 else:
     listen_port = 5000
+
+if 'QV_BASE_URL' in os.environ:
+    base_url = os.environ['QV_BASE_URL']
+else:
+    base_url = "http://localhost:%d/" % listen_port
 
 # dummy data to stop proxy buffering
 dummy_data = range(0, 20480)
